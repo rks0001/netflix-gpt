6c90f126814ef5360343382b27ab709c3b129a13
Ranjit Kalita <ranjitkalita499@gmail.com>
2023-09-29 01:08:20 +0530

    changes

--- src/hooks/useNowPlayingMovies.js
+++ src/hooks/useNowPlayingMovies.js
@@ -1,4 +1,4 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addNowPlayingMovies } from "../utils/moviesSlice";
 import { useEffect } from "react";
 import {API_OPTIONS} from "../utils/constants"
@@ -7,7 +7,7 @@ import {API_OPTIONS} from "../utils/constants"
 const useNowPlayingMovies = () =>{
     const dispatch = useDispatch(); 
 
-    // const nowPlayingMovies = useSelector(store => store.movies.nowPlayingMovies); 
+    const nowPlayingMovies = useSelector(store => store.movies.nowPlayingMovies); 
 
 
     const getNowPlayingMovies = async() =>{
@@ -20,7 +20,7 @@ const useNowPlayingMovies = () =>{
     }
  
     useEffect(()=>{
-    getNowPlayingMovies(); 
+    !nowPlayingMovies && getNowPlayingMovies(); 
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 }
